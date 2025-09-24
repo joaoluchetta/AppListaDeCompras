eb7447b224716ea700f629d89383b8d34185fe9c
joaoluchetta <joaoluchetta.dev@gmail.com>
2025-09-24 17:57:32 -0300

    ADD telas de cadastro usuario e home

--- app/src/main/java/com/example/listadecompras/MainActivity.kt
+++ app/src/main/java/com/example/listadecompras/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.listadecompras
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.listadecompras.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnAcessar.setOnClickListener {
+            val intentHome = Intent(this, HomeActivity::class.java)
+            startActivity(intentHome)
+        }
+
+        binding.btnCadastro.setOnClickListener {
+            val intentCadastroConta = Intent(this, ContaCadastroActivity::class.java)
+            startActivity(intentCadastroConta)
         }
     }
 }
